9714d2464c792be306541b38db55a2380d14f678
Geetesh Kalakoti <kalakotig@gmail.com>
2015-07-10 20:54:26 +0100

    More debugging commands

--- ndk.sh
+++ ndk.sh
@@ -4,5 +4,4 @@ rm -rf android-ndk-r10e
 wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
 chmod a+x *.bin
 ./*.bin > null
-rm *.bin
-ls
+ls -l android-ndk-r10e/toolchains/llvm-3.6/prebuilt/linux-x86_64/bin
