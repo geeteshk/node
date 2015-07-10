5e92495b10bf9b43ebce1a73ea02f1ceed8a075e
Geetesh Kalakoti <kalakotig@gmail.com>
2015-07-10 21:09:22 +0100

    Check for storage overflows

--- ndk.sh
+++ ndk.sh
@@ -2,6 +2,7 @@
 
 rm -rf android-ndk-r10e
 wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+df -h
 chmod a+x *.bin
 ./*.bin > null
-ls -l android-ndk-r10e/toolchains/llvm-3.6/prebuilt/linux-x86_64/bin
+df -h
