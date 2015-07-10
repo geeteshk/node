5ae254e77418ce684a05547a7a773abda7f8a793
Geetesh Kalakoti <kalakotig@gmail.com>
2015-07-10 12:15:11 +0100

    Always download ndk

--- ndk.sh
+++ ndk.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -d "android-ndk-r10e" ]; then
-  wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
-  chmod a+x *.bin
-  ./*.bin
-  rm *.bin
-fi
+rm -rf android-ndk-r10e
+wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+chmod a+x *.bin
+./*.bin
+rm *.bin
